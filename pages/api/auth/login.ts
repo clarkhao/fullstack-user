6084491de28f95f1a7e71cdc59ddd35b95cf0497
clarkhao <clarktotoro@gmail.com>
2022-11-19 16:03:18 +0800

    add swagger

---
+++ pages/api/auth/login.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     description: log in with user info
+ * 
+ */
---
+++ pages/api/auth/logout.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+/**
+ * @swagger
+ * /api/auth/logout:
+ *   post:
+ *     description: log out
+ * 
+ */
